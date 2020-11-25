186f40f95c37cdd6840277714bf0f75992e41baa
Quit3Simpl3 <abramovichavishag@gmail.com>
2020-11-25 19:51:45 +0200

    added notes to MessageBusImpl

--- src/main/java/bgu/spl/mics/MessageBusImpl.java
+++ src/main/java/bgu/spl/mics/MessageBusImpl.java
@@ -6,11 +6,17 @@ package bgu.spl.mics;
  * Only private fields and methods can be added to this class.
  */
 public class MessageBusImpl implements MessageBus {
-	
+	// TODO: fields
+	// main-queue: stores the messages sent to the MB for processing.
+	// dictionary of (key=Event type (Event.class), value=Event) that holds the events the events sent to the MB
+	// dictionary of (key=MicroService, value=queue of messages) (NOT in MicroService class! See page 8.
+	// add another dictionary for broadcast messages? or: when awaitMessage(), check if the type of the next message is broadcast -
+	// 		if it is a broadcast message, don't pop it from the msg queue, but add it to a popping-list that will pop all the
+	//		broadcast messages when the round-robin round ends.
 	
 	@Override
 	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
-		
+		// TODO: add m to (Event type, MicroService)[type] queue
 	}
 
 	@Override
