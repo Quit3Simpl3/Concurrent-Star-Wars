2e270208e968adaa821922f925cc37dfe69c2e70
Quit3Simpl3 <abramovichavishag@gmail.com>
2020-11-27 12:35:11 +0200

    Added testSendEvent

--- src/test/java/bgu/spl/mics/MessageBusImplTest.java
+++ src/test/java/bgu/spl/mics/MessageBusImplTest.java
@@ -3,6 +3,7 @@ package bgu.spl.mics;
 import bgu.spl.mics.application.messages.AttackEvent;
 import bgu.spl.mics.application.services.*;
 import bgu.spl.mics.example.messages.ExampleBroadcast;
+import bgu.spl.mics.example.messages.ExampleEvent;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,11 +19,15 @@ class MessageBusImplTest {
     MicroService han;
     MicroService lando;
     Broadcast broadTest;
+    Event exampleEvent1, exampleEvent2, exampleEvent3;
 
     @BeforeEach
     void setUp() {
         testMsg1 = null;
         testMsg2 = null;
+        exampleEvent1 = new ExampleEvent("test");
+        exampleEvent2 = new ExampleEvent("test");
+        exampleEvent3 = new ExampleEvent("test");
         mb = new MessageBusImpl();
         c3po = new C3POMicroservice();
         han = new HanSoloMicroservice();
@@ -35,55 +40,111 @@ class MessageBusImplTest {
     }
 
     @Test
-    void subscribeEvent() {
+    void testSubscribeEvent() {
     }
 
     @Test
-    void subscribeBroadcast() {
+    void testSubscribeBroadcast() {
     }
 
     @Test
-    void complete() {
+    void testComplete() {
     }
 
     @Test
-    void sendBroadcast() {
-        // Register han and c3po to the message bus:
+    void testSendBroadcast() {
+        // Register Han and C3PO to the message bus:
         mb.register(han);
         mb.register(c3po);
-
+        // Subscribe Han and C3PO to broadTest in the message bus:
         mb.subscribeBroadcast(broadTest.getClass(),han);
         mb.subscribeBroadcast(broadTest.getClass(),c3po);
         // Test awaitMessage without a message waiting:
         try {
             testMsg1 = mb.awaitMessage(han);
-        } catch (InterruptedException e){
+            testMsg2 = mb.awaitMessage(c3po);
+        }
+        catch (InterruptedException e) {
             assertNull(testMsg1);
-        };
+            assertNull(testMsg2);
+        }
         // Test awaitMessage with a message waiting:
         mb.sendBroadcast(broadTest);
         try {
-            testMsg1 = mb.awaitMessage(han);
-            assertEquals(((ExampleBroadcast)testMsg1).getSenderId(),((ExampleBroadcast)broadTest).getSenderId());
+            testMsg1 = mb.awaitMessage(han); // Receive the broadcast for han
+            testMsg2 = mb.awaitMessage(c3po); // Receive the broadcast for c3po
         }
         catch (InterruptedException e) {
             fail(); // Han should receive a message now, otherwise something is wrong.
         }
-
-
+        // Assert that both han AND c3po received the broadcast message:
+        assertEquals(
+                ((ExampleBroadcast)testMsg1).getSenderId(),
+                ((ExampleBroadcast)broadTest).getSenderId()
+        );
+        assertEquals(
+                ((ExampleBroadcast)testMsg2).getSenderId(),
+                ((ExampleBroadcast)broadTest).getSenderId()
+        );
     }
 
     @Test
-    void sendEvent() {
-        //TODO :: COPY FROM BROADCAST
+    void testSendEvent() {
+        // Register Han and C3PO to the message bus:
+        mb.register(han);
+        mb.register(c3po);
+        // Subscribe Han and C3PO to AttackEvent type in the message bus:
+        mb.subscribeEvent(AttackEvent.class, han);
+        mb.subscribeEvent(AttackEvent.class, c3po);
+        // Test awaitMessage without a message waiting:
+        try {
+            testMsg1 = mb.awaitMessage(han);
+        }
+        catch (InterruptedException e) {
+            assertNull(testMsg1);
+        }
+        // Test awaitMessage with ONE message waiting:
+        mb.sendEvent(exampleEvent1);
+        try {
+            testMsg1 = mb.awaitMessage(han);
+            testMsg2 = mb.awaitMessage(c3po);
+        }
+        catch (InterruptedException e) {
+            // Make sure han received a message:
+            assertEquals(
+                    ((ExampleEvent)testMsg1).getSenderName(),
+                    ((ExampleEvent)exampleEvent1).getSenderName()
+                );
+            // Make sure c3po didn't receive a message (Event is received only once):
+            assertNull(testMsg2);
+        }
+        // Test awaitMessage with TWO messages waiting:
+        mb.sendEvent(exampleEvent2);
+        mb.sendEvent(exampleEvent3);
+        try {
+            testMsg1 = mb.awaitMessage(han);
+            testMsg2 = mb.awaitMessage(c3po);
+        }
+        catch (InterruptedException e) {
+            fail(); // If the awaitMessage didn't return a message twice it therefore failed to work propertly.
+        }
+        // Make sure han AND c3po received each a single, different message:
+        assertEquals(
+                ((ExampleEvent)testMsg1).getSenderName(),
+                ((ExampleEvent)exampleEvent2).getSenderName()
+        );
+        assertEquals(
+                ((ExampleEvent)testMsg2).getSenderName(),
+                ((ExampleEvent)exampleEvent3).getSenderName()
+        );
     }
 
     @Test
-    void register() {
+    void testRegister() { // TODO: Check whether we need to test this.
     }
 
 
     @Test
-    void awaitMessage() {
+    void testAwaitMessage() {
     }
 }
