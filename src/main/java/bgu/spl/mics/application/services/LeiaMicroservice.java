cf1b645d8dbe07ff8c737f59a78b9ce0a8a227a2
Quit3Simpl3 <abramovichavishag@gmail.com>
2020-11-27 17:08:10 +0200

    import Attack and AttackEvent to Leia

--- src/main/java/bgu/spl/mics/application/services/LeiaMicroservice.java
+++ src/main/java/bgu/spl/mics/application/services/LeiaMicroservice.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import bgu.spl.mics.MicroService;
+import bgu.spl.mics.application.passiveObjects.Attack;
+import bgu.spl.mics.application.messages.AttackEvent;
 
 /**
- * LeiaMicroservices Initialized with Attack objects, and sends them as  {@link AttackEvents}.
+ * LeiaMicroservices Initialized with Attack objects, and sends them as  {@link AttackEvent}s.
  * This class may not hold references for objects which it is not responsible for:
- * {@link AttackEvents}.
+ * {@link AttackEvent}s.
  *
  * You can add private fields and public methods to this class.
  * You MAY change constructor signatures and even add new public constructors.
