afe4cc18f660c015e7b19d9c86e6a940c60d4833
assafrbi <assafrbi@gmail.com>
2020-12-03 10:56:50 +0200

    50% from buslmpl (subscribe+regi+builder+compl)

--- src/main/java/bgu/spl/mics/MessageBusImpl.java
+++ src/main/java/bgu/spl/mics/MessageBusImpl.java
@@ -1,7 +1,8 @@
 package bgu.spl.mics;
 
-import java.util.HashMap;
-import java.util.Queue;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * The {@link MessageBusImpl class is the implementation of the MessageBus interface.
@@ -19,33 +20,57 @@ public class MessageBusImpl implements MessageBus {
 	//		broadcast messages when the round-robin round ends.
 	private static MessageBusImpl instance = null; // Implemented as Singleton
 
-	private HashMap<Class<? extends Event>, Queue> EventHash;
+
+	private Map<Class<? extends Event>, ConcurrentLinkedQueue> eventHash;
+	private Map<Class<? extends Broadcast>, ConcurrentLinkedQueue> broadcastHash;
+	private Map<MicroService, ConcurrentLinkedQueue> microServiceHash;
+	private Map<Event, Future> completeFutre;
 
 	private MessageBusImpl() { // private constructor for singleton
 		// TODO: initialize fields
+		eventHash = new ConcurrentHashMap<Class<? extends Event>, ConcurrentLinkedQueue>() {};
+		broadcastHash = new ConcurrentHashMap<Class<? extends Broadcast>, ConcurrentLinkedQueue>() {};
+		microServiceHash = new ConcurrentHashMap<MicroService, ConcurrentLinkedQueue>() {};
+		completeFutre = new ConcurrentHashMap<Event, Future>();
 	}
 
 	// static method to create instance
 	public static MessageBusImpl getInstance() {
 		if (instance == null)
-			instance = new MessageBusImpl();
-
+			instance = new MessageBusImpl();;
 		return instance;
 	}
 
 	@Override
 	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
-		// TODO: add m to (Event type, MicroService)[type] queue
+		if (eventHash == null || eventHash.get(type) == null) {
+			ConcurrentLinkedQueue<MicroService> eventSubscribList = new ConcurrentLinkedQueue<MicroService>();
+			eventHash.put(type,eventSubscribList);
+		}
+		if(!eventHash.get(type).contains(m)) { //TODO : we need to know if i can add twice
+			eventHash.get(type).add(m);
+		}
 	}
 
 	@Override
 	public void subscribeBroadcast(Class<? extends Broadcast> type, MicroService m) {
-		
+		if (broadcastHash == null || broadcastHash.get(type) == null) {
+			ConcurrentLinkedQueue<MicroService> broadcastSubscribList = new ConcurrentLinkedQueue<MicroService>();
+			broadcastHash.put(type,broadcastSubscribList);
+		}
+		if(!broadcastHash.get(type).contains(m)) { //TODO : we need to know if i can add twice
+			broadcastHash.get(type).add(m);
+		}
     }
 
 	@Override @SuppressWarnings("unchecked")
 	public <T> void complete(Event<T> e, T result) {
-		
+		if (completeFutre.get(e) != null) { //not sure its can be..
+			completeFutre.get(e).resolve(result);
+
+			// not sure what to do with the result
+		}
+
 	}
 
 	@Override
@@ -62,11 +87,13 @@ public class MessageBusImpl implements MessageBus {
 
 	@Override
 	public void register(MicroService m) {
-		
+		ConcurrentLinkedQueue<Message> iRegister = new ConcurrentLinkedQueue();
+		microServiceHash.put(m,iRegister);
 	}
 
 	@Override
 	public void unregister(MicroService m) {
+		microServiceHash.remove(m);  //TODO : need to remove from broadcast and event and what to do with missions i didnt finish
 		
 	}
 
