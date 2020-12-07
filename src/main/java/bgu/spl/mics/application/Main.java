41056551ea722949b86979bb264b88a801fd031b
Quit3Simpl3 <abramovichavishag@gmail.com>
2020-12-07 13:30:57 +0200

    Main: added parseJson and generateDiaryOutput

--- src/main/java/bgu/spl/mics/application/Main.java
+++ src/main/java/bgu/spl/mics/application/Main.java
@@ -1,7 +1,10 @@
 package bgu.spl.mics.application;
 
-import bgu.spl.mics.application.passiveObjects.Ewoks;
-import bgu.spl.mics.application.passiveObjects.Input;
+import bgu.spl.mics.application.passiveObjects.*;
+import com.google.gson.Gson;
+
+import java.io.FileWriter;
+import java.io.IOException;
 
 /** This is the Main class of the application. You should parse the input file,
  * create the different components of the application, and run the system.
@@ -12,10 +15,49 @@ public class Main {
 		// Create the Ewoks:
 		Ewoks ewoks = Ewoks.getInstance();
 		ewoks.createEwoks(input.getEwoks());
+		// Create the diary:
+		Diary diary = Diary.getInstance();
+		// Create the attacks:
+		Attack[] attacks = input.getAttacks();
+		// TODO: Set attack sleep() times (millis):
+
+		// TOOD: Create microservices
+
+	}
+
+	private static Input parseJson(String path) throws IOException {
+		return JsonInputReader.getInputFromJson(path);
+	}
+
+	private static void generateDiaryOutput(String path) throws IOException {
+		Gson gson = new Gson();
+		// TODO: Verify this output!
+		Diary diary = Diary.getInstance();
+		gson.toJson(diary, new FileWriter(path));
 	}
 
 	public static void main(String[] args) {
-		Input input = new Input();
+		String inputPath = args[0];
+		String outputPath = args[1];
+		Input input = null;
+		try {
+			input = parseJson(inputPath);
+			if (input == null) throw new IOException();
+		}
+		catch (IOException e) {
+			System.out.println("Json parsing error. Check the file.");
+			System.out.println(e.getMessage());
+		}
 		initialize(input);
+		// TODO: Start the microservices:
+
+		// Finally:
+		try {
+			generateDiaryOutput(outputPath);
+		}
+		catch (IOException e) {
+			System.out.println("Json writing error. Check the file.");
+			System.out.println(e.getMessage());
+		}
 	}
 }
