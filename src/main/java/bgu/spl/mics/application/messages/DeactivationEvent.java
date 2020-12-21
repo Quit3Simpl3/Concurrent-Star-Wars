0084ee77055903449b2f1e176b7e3cfd1bc82d0f
Quit3Simpl3 <21604833+Quit3Simpl3@users.noreply.github.com>
2020-12-21 09:41:51 +0200

    Update DeactivationEvent.java

--- src/main/java/bgu/spl/mics/application/messages/DeactivationEvent.java
+++ src/main/java/bgu/spl/mics/application/messages/DeactivationEvent.java
@@ -1,5 +1,4 @@
 package bgu.spl.mics.application.messages;
 import bgu.spl.mics.Event;
 
-public class DeactivationEvent implements Event<Boolean> {
-}
+public class DeactivationEvent implements Event<Boolean> {}
