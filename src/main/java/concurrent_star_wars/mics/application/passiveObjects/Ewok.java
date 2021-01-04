84927dcfc283a19ac2d9a47a92371e01034b830f
Quit3Simpl3 <21604833+Quit3Simpl3@users.noreply.github.com>
2021-01-04 19:50:34 +0200

    Update Ewok.java

--- src/main/java/concurrent_star_wars/mics/application/passiveObjects/Ewok.java
+++ src/main/java/concurrent_star_wars/mics/application/passiveObjects/Ewok.java
@@ -19,26 +19,26 @@ public class Ewok {
      */
     public void acquire() {
 		this.available = false;
-    } // DO NOT CHANGE!
+    }
 
     /**
      * release an Ewok
      */
     public void release() {
     	this.available = true;
-    } // DO NOT CHANGE!
+    }
 
     /*
     * Check if the Ewok is available for acquiring
      */
     public boolean isAvailable() {
         return available;
-    } // DO NOT CHANGE!
+    }
 
     /*
     * Return the Ewok's serial number
      */
     public int getSerialNumber() {
         return this.serialNumber;
-    } // DO NOT CHANGE!
+    }
 }
